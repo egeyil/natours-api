05a86b58493222e725f6c49ebe6bd0a2a5b9580d
egeyil <egeyildirimdevelopment@gmail.com>
2022-07-20 23:04:47 +0300

    add eslint and relevant dependencies

--- app.js
+++ app.js
@@ -1,24 +1,21 @@
-const express = require("express");
-const morgan = require("morgan");
+const express = require('express');
+const morgan = require('morgan');
 
 const app = express();
 
-const tourRouter = require("./routes/tourRoutes");
-const userRouter = require("./routes/userRoutes");
+const tourRouter = require('./routes/tourRoutes');
+const userRouter = require('./routes/userRoutes');
 
 // MIDDLEWARES
-app.use(morgan("dev"));
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 
-app.use((req, res, next) => {
-  console.log("Hello from the middleware!");
-  next();
-});
-
 // ROUTES
 // This is called "mounting" the router
-app.use("/api/v1/tours", tourRouter);
-app.use("/api/v1/users", userRouter);
+app.use('/api/v1/tours', tourRouter);
+app.use('/api/v1/users', userRouter);
 
 module.exports = app;
